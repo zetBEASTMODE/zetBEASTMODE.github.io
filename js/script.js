e747bafdf5887e840ab3b7732e44bc8423c3aed0
zetBEASTMODE <101716645+zetBEASTMODE@users.noreply.github.com>
2022-06-03 21:55:50 +1000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,69 @@
+/* $(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow/right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                dots: true,
+                arrows: false
+                }
+            }
+        ]
+      });
+  }); */ /* slick-slider jQuery */
+
+
+  const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    responsive: {
+        320: {
+            nav: true,
+            navPosition: "bottom"
+        },
+        1100: {
+            nav: false,
+        }
+    }
+  });
+
+  document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+  });
+
+  document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+  });  /* tiny-slider */
+
+
+
+$(document).ready(function(){      
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+
+
+
+
+      function toggleSLide(item) {
+        $(item).each(function(i) {
+          $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+          })
+        });
+      };
+
+      toggleSLide('.catalog-item__link');
+      toggleSLide('.catalog-item__back');
+});
+
